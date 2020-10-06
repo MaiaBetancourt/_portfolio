601cda5866ad5a849d1ee962b5f8e4c2d1fd56eb
Maria <maria.betancourt15@yahoo.com>
2020-10-05 23:09:03 -0700

    added the link to the about, contact, anp portfolio pages

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,9 +1,28 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
   return (
-    <h1>Welcome tp my app!!!</h1>
-  );
+    <BrowserRouter>
+    <div>
+      <Route exact path="/" component={About} />
+      <Route exact path="/Portfolio" component={Portfolio} />
+      <Route exact path="/Contact" component={Contact} />
+    </div>
+    </BrowserRouter>
+  )
+}
+
+function About() {
+  return <h1>Welcome to about!!!</h1>;
+}
+
+function Portfolio() {
+  return <h1>Welcome to portfolio!!!</h1>;
+}
+
+function Contact() {
+  return <h1>Welcome to Contact!!!</h1>;
 }
 
 export default App;
