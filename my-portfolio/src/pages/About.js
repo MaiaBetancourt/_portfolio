72af63dcda3df603e172a111842eb0eba27d5bf3
Maria <maria.betancourt15@yahoo.com>
2020-10-07 19:44:55 -0700

    added the updated about me

--- my-portfolio/src/pages/About.js
+++ my-portfolio/src/pages/About.js
@@ -20,11 +20,19 @@ function About() {
         <div className="row">
           <div className="col col-md-6">
             <p>
-              My name is Maria Betancourt and I am entirely new to the use of
-              computers let alone coding. I was born in Acapulco, Mexico and I
-              moved to Las Vegas when i was ten years old. I love the outdoors
-              like camping, hiking, reading, and going on long drives. I am
-              excited to see how much I will learn taking this course.
+              My name is Maria. I recently received a certificate in Full
+              Stack Web Development from University of Arizona Coding Bootcamp.
+              During the course, I learned to develop projects in groups,
+              delegate tasks, and meet deadlines. I acquired skills in both
+              front and back-end technologies, learned languages like HTML, CSS,
+              and JavaScript, frameworks like React and Express, and databases
+              like MySQL and MongoDB.
+            </p>
+            <p>
+              In the past I've worked managing digital presses which highly
+              developed my critical thinking skills and attention to detail.
+              I've provided links tol some of my projects and also contact
+              information should there be any questions.
             </p>
           </div>
           <div className="col col-md-6">
