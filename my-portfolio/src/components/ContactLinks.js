86acfbf4fa27a06e1e6742c364ac3d67b128af62
Maria <maria.betancourt15@yahoo.com>
2020-10-07 19:29:01 -0700

    added the linked in

--- my-portfolio/src/components/ContactLinks.js
+++ my-portfolio/src/components/ContactLinks.js
@@ -3,11 +3,11 @@ import React from "react";
 export default function ContactLinks() {
   return (
     <ul className="list-group list-group-flush text-center">
-      <li className="list-group-item"> <a href="">LinkedIn</a> </li>
-      <li className="list-group-item"> <a href="mailto:maria.betancourt15@yahoo.com">Email</a> </li>
-      <li className="list-group-item"> <a href="">My Resume</a> </li>
-      <li className="list-group-item"> <a href="">Phone Number</a></li>
+      <li className="list-group-item"> <a href="https://www.linkedin.com/in/maria-betancourt-ramirez-06b4971b3/">LinkedIn</a> </li>
+      <li className="list-group-item"> <a href="mailto:maria.betancourt15@yahoo.com" target="_blank">Email</a> </li>
+      <li className="list-group-item"> <a href="https://docs.google.com/document/d/1wa0Krgu8jXRMvIkTau72gDp7ckSznrpcC2tSVo24vRo/edit?usp=sharing">My Resume</a> </li>
       <li className="list-group-item"> <a href="https://github.com/MaiaBetancourt">Github</a></li>
+      <li className="list-group-item"> <a href="">1-702-626-6346</a></li>
     </ul>
   );
 }
