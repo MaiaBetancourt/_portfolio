431239cdb8cc01cda4cd0e46af938faa4d478967
Maria <maria.betancourt15@yahoo.com>
2020-10-07 22:24:29 -0700

    added the footer

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
@@ -12,6 +13,7 @@ function App() {
         <Navbar />
         <Route exact path="/" component={About} />
         <Route exact path="/Portfolio" component={Portfolio} />
+        <Footer />
       </div>
     </BrowserRouter>
   );
---
+++ my-portfolio/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div>
+      <footer className="footer mt-auto py-3">
+        <div className="container">
+        <span className="text-muted">Maria Betancourt, 2020</span>
+        </div>
+      </footer>
+    </div>
+  );
+}
