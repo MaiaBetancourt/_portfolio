84de5c49d129e4c84e8d3570edc4c29e793ac7d2
Maria <maria.betancourt15@yahoo.com>
2020-10-07 23:59:44 -0700

    centered the footer

--- my-portfolio/src/components/Footer.js
+++ my-portfolio/src/components/Footer.js
@@ -3,8 +3,8 @@ import React from "react";
 export default function Footer() {
   return (
     <div>
-      <footer className="footer mt-auto py-3">
-        <div className="container">
+      <footer className="footer mt-auto py-3 alert-dark">
+        <div className="container text-center">
         <span className="text-muted">Maria Betancourt, 2020</span>
         </div>
       </footer>
