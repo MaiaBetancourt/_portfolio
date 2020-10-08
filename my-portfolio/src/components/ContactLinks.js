2e030b8d4a3e0ceb6c2398342a34af3f8480fc9c
Maria <maria.betancourt15@yahoo.com>
2020-10-08 01:02:50 -0700

    eslint did not work

--- my-portfolio/src/components/ContactLinks.js
+++ my-portfolio/src/components/ContactLinks.js
@@ -4,10 +4,10 @@ export default function ContactLinks() {
   return (
     <ul className="list-group list-group-flush text-center">
       <li className="list-group-item"> <a href="https://www.linkedin.com/in/maria-betancourt-ramirez-06b4971b3/">LinkedIn</a> </li>
-      {/* <li className="list-group-item"> <a href="mailto:maria.betancourt15@yahoo.com">Email</a> </li>  */}
+      <li className="list-group-item"> <a href="mailto:maria.betancourt15@yahoo.com">Email</a> </li> 
       <li className="list-group-item"> <a href="../images/MariaBetancourtResume.pdf" download>My Resume</a> </li>
       <li className="list-group-item"> <a href="https://github.com/MaiaBetancourt">Github</a></li>
-      {/* <li className="list-group-item"> <a href="#">1-702-626-6346</a></li>  */}
+      <li className="list-group-item"> <a href="#">1-702-626-6346</a></li> 
     </ul>
   );
 }
