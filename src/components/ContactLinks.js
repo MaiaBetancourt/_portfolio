07ce53c586de37e5f131d2504e321e67b763178b
Maria <maria.betancourt15@yahoo.com>
2020-10-08 10:56:46 -0700

    added gmail instead of yahoo

--- src/components/ContactLinks.js
+++ src/components/ContactLinks.js
@@ -5,7 +5,7 @@ export default function ContactLinks() {
     <ul className="list-group list-group-flush text-center">
       <li className="list-group-item"> <a href="https://www.linkedin.com/in/maria-betancourt-ramirez-06b4971b3/">LinkedIn</a> </li>
       {/* eslint-disable-next-line */}
-      <li className="list-group-item"> <a href="mailto:maria.betancourt15@yahoo.com">Email</a> </li> 
+      <li className="list-group-item"> <a href="mailto:betancourt.maia@gmail.com">Email</a> </li> 
       <li className="list-group-item"> <a href="../images/MariaBetancourtResume.pdf" download>My Resume</a> </li>
       <li className="list-group-item"> <a href="https://github.com/MaiaBetancourt">Github</a></li>
       {/* eslint-disable-next-line */}
