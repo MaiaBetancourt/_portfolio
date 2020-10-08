8f22c07f14c1a7edd12b8dea85e89f4d8704a9bb
Maria <maria.betancourt15@yahoo.com>
2020-10-07 21:52:40 -0700

    added the resume pdf

--- my-portfolio/src/components/ContactLinks.js
+++ my-portfolio/src/components/ContactLinks.js
@@ -5,7 +5,7 @@ export default function ContactLinks() {
     <ul className="list-group list-group-flush text-center">
       <li className="list-group-item"> <a href="https://www.linkedin.com/in/maria-betancourt-ramirez-06b4971b3/">LinkedIn</a> </li>
       <li className="list-group-item"> <a href="mailto:maria.betancourt15@yahoo.com" target="_blank">Email</a> </li>
-      <li className="list-group-item"> <a href="https://docs.google.com/document/d/1wa0Krgu8jXRMvIkTau72gDp7ckSznrpcC2tSVo24vRo/edit?usp=sharing">My Resume</a> </li>
+      <li className="list-group-item"> <a href="../images/MariaBetancourtResume.pdf" download>My Resume</a> </li>
       <li className="list-group-item"> <a href="https://github.com/MaiaBetancourt">Github</a></li>
       <li className="list-group-item"> <a href="">1-702-626-6346</a></li>
     </ul>
